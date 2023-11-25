f68408c101eee2da3e7bdc0b62500bf8a473c94c
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2023-11-25 20:28:32 +0500

    Updated For Dashboard

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import { Button } from "@nextui-org/button";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button color="primary">Click me</Button>
+      <Link href={"/dashboard"}>
+        {" "}
+        <Button color="primary">Move to /dashboard</Button>
+      </Link>
     </main>
   );
 }
